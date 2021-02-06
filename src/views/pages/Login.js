cf80eaa48ca991769f07941d0c3906a534702e1c
Raphael Borges <rah-borges@hotmail.com>
2021-02-06 18:07:36 -0300

    style(login):estilizado

--- src/views/pages/Login.js
+++ src/views/pages/Login.js
@@ -4,16 +4,23 @@ let Login  = {
     render: async () =>{
         let NavBar = await Nav.render()
         let view = `
+        <div class="container card col-4 p-3 mt-5">
         <h1>It's Login Bitch</h1> 
         <form action="#/dashboard">
-            <label for="usuario">Usuário</label>
-            <input type="text" name="usuario" id="" placeholder="Digite seu usuário">
-            <label for="Senha">Senha</label>
-            <input type="password" name="senha" id="" placeholder="Digite sua senha">
-            <button type="submit">Login</button>
+            <div class="mb-3">
+                <label for="usuario" class="form-label">Usuario</label>
+                <input type="text" class="form-control" id="usuario" aria-describedby="emailHelp">
+                <div id="emailHelp" class="form-text">Digite o seu usário.</div>
+            </div>
+            <div class="mb-3">
+                <label for="senha" class="form-label">Senha</label>
+                <input type="senha" class="form-control" id="senha">
+            </div>
+            <button type="submit" class="btn btn-primary">Entrar</button>
         </form>
         <a href="#/signup">Cadastrar</a>
         <a href="#/forgotpass">Esqueci a senha</a>
+        </div>
         `
         return view
     },
