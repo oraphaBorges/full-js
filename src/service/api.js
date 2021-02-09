d0ba0965c4ddb7da82519f6dd6d2b197265c75e5
Raphael Borges <rah-borges@hotmail.com>
2021-02-08 21:23:10 -0300

    refac(post):cadastro

--- src/service/api.js
+++ src/service/api.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
+    baseURL:'https://accenture-java-desafio.herokuapp.com/'
     // baseURL:'https://gama-bankline-desbugados.herokuapp.com/'
-    baseURL:'https://webhook.site/70ecbcbf-8a13-4e2d-8633-dc76c6025d8b'
+    // baseURL:'https://webhook.site/70ecbcbf-8a13-4e2d-8633-dc76c6025d8b'
 })
 
 export default api;
--- src/views/pages/Home.js
+++ src/views/pages/Home.js
@@ -1,31 +1,61 @@
 import api from '../../service/api';
 import Nav from '../components/Nav';
+import Logo from '../../img/logo.png';
 
 let Home  = {
     render: async () =>{
         let NavBar = await Nav.render()
-        let view = `<h1>Home page</h1>
-            <div>
-                <a href="#/login">Logar</a>
-                <a href="#/signup">Cadastrar</a>
+        let view = `
+            <nav class="nav container">
+                <a class="logo" href="#">
+                    <img class="nav-logo" src="${Logo}" alt="logo">
+                </a>
+            </nav>
+            <div class="login">
+                <h2>Faça o seu Cadastro</h2>
+                <p class="text-left">Empreencha seu dados para realizar seu cadastro</p>
+                <form id="form" class="login-form">
+                    <input id="cpf" class="input" type="text" name="cpf" placeholder="CPF">
+                    <input id="name" class="input" type="text" name="name" placeholder="Nome completo">
+                    <input id="login" class="input" type="text" login="login" placeholder="Login">
+                    <input id="password" class="input" type="password" name="password" placeholder="Digite sua senha">
+                    <input id="confirm-password" class="input" type="password" name="confirm-password" placeholder="Confirme sua senha">
+                    <button id="bnt" type="submit" class="btn btn-main-outline btn-rounded">Cadastrar</button>
+                </form>
+                <a href="/login.html">Fazer Login</a>
             </div>
         `
         return view
     },
     after_render: async () =>{
-        var buttonTest = document.getElementById('tester');
-        buttonTest.addEventListener('click', function(){
+        var form = document.getElementById('form');
+        form.addEventListener('submit', function(e){
+            e.preventDefault()
             let postData = {
-                name:'Cris Henrique',
-                age:31
+                 cpf: document.getElementById('cpf').value,
+                 login:document.getElementById('login').value,
+                 nome: document.getElementById('name').value,
+                 senha: document.getElementById('password').value
             }
-            // api.post('',postData).then(res => {
-            //     console.log(res.data)
-            // })
-
-            localStorage.setItem('dados',JSON.stringify(postData))
-        })
-    }
+            let confirm = document.getElementById('password').value
+            if(postData.senha.length >=6 && postData.senha === confirm){
+                api.post('usuarios',postData, {
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                }).then(response =>{
+                    alert('Usuário cadastrado')
+                    console.log(response);
+                    location.replace('#/login');
+                }).catch(response => {
+                    console.log(response);
+                })
+            }else{
+                location.replace('#/login');
+            }   
+        }
+    )}
 }
+
 
 export default Home;
